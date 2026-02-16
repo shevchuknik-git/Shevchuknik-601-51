451dad7056c30a9d31ab11c4409719d7d56b1235
shevchuknik-git <shevchushanik@gmail.com>
2026-02-16 16:22:54 +0500

    Первая редация кода 00_distance

---
+++ lab_1/T0D0.py
@@ -0,0 +1,22 @@
+from math import *
+sites = {
+    'Moscow': (550, 370),
+    'London': (510, 510),
+    'Peris': (480, 480),
+}
+distance = {}
+
+cities = list(sites.keys())
+
+for i in range (len(cities)):
+ for j in range (i + 1, len(cities)):
+  city_1 = cities[i]
+  city_2 = cities[j]
+
+  x1, y1 = (sites[city_1])
+  x2, y2 = (sites[city_2])
+
+  dist =  sqrt((x1 - x2)**2 +  (y1 - y2)**2)
+  distance[(city_1, city_2)] = dist
+
+print(distance)

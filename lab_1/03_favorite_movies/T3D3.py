267a5f4dd0b674adca6317279f83c7a6a10ec3e7
shevchuknik-git <shevchushanik@gmail.com>
2026-02-20 21:10:29 +0500

    Add: Created files for lab_1\03_favorite_movies

---
+++ lab_1/03_favorite_movies/T3D3.py
@@ -0,0 +1,6 @@
+my_favorite_movies = "Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее"
+
+print(my_favorite_movies[:10])
+print(my_favorite_movies[-15:])
+print(my_favorite_movies[12:25])
+print(my_favorite_movies[-22:-17])

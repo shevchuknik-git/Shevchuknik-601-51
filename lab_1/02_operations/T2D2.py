866db7ab23039aede9e11f4b714159a5e6add056
shevchuknik-git <shevchushanik@gmail.com>
2026-02-26 11:45:33 +0500

    Add: Created files for lab_1/02_operations

--- lab_1/02_operations/T2D2.py
+++ lab_1/02_operations/T2D2.py
@@ -1,9 +1,18 @@
-from itertools import *
+from itertools import product
 
-num = ['1','2','3','4','5']
-ops = ['+','-','*']
+num = ['1', '2', '3', '4', '5']
+ops = ['+', '-', '*']
+solutions = []
 
 for comb in product(ops, repeat=4):
     exp = f"(({num[0]}{comb[0]}{num[1]}{comb[1]}{num[2]}){comb[2]}{num[3]}){comb[3]}{num[4]}"
-    if eval(exp) == 25:  
-        print(exp, '= 25')
+    result = eval(exp)
+    if result == 25:
+        solutions.append((exp, result))
+
+if solutions:
+    print(f"Найдено {len(solutions)} решений:")
+    for exp, res in solutions:
+        print(f"{exp} = {res}")
+else:
+    print("Решений не найдено.")
